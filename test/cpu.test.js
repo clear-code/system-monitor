c8279888bfc7dd623ac8cb3a069080d6fc55d9d4
Hiroyuki Ikezoe <poincare@ikezoe.net>
2009-10-06 10:51:15 +0900

    accessor tests.

--- test/cpu.test.js
+++ test/cpu.test.js
@@ -9,8 +9,30 @@ function tearDown() {
 }
 
 testCreate.description = "create instance test";
-
+testCreate.priority = 'must';
 function testCreate() {
-  gCPU = Cc["@clear-code.com/cpu/monitor;1"].createInstance().QueryInterface(Ci.clICPUMonitor);
+  gCPU = Cc["@clear-code.com/cpu/monitor;1"].createInstance(Ci.clICPUMonitor);
+  assert.isDefined(gCPU);
+}
+
+testUser.description = "user property test";
+testUser.priority = 'must';
+function testUser() {
+  gCPU = Cc["@clear-code.com/cpu/monitor;1"].createInstance(Ci.clICPUMonitor);
+  assert.equal(0, gCPU.user);
+}
+
+testSystem.description = "system property test";
+testSystem.priority = 'must';
+function testSystem() {
+  gCPU = Cc["@clear-code.com/cpu/monitor;1"].createInstance(Ci.clICPUMonitor);
+  assert.equal(0, gCPU.system);
+}
+
+testIdle.description = "idle property test";
+testIdle.priority = 'must';
+function testIdle() {
+  gCPU = Cc["@clear-code.com/cpu/monitor;1"].createInstance(Ci.clICPUMonitor);
+  assert.equal(0, gCPU.idle);
 }
 
