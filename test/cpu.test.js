2c859d59a973a306c63cbc88c27943230c4ee266
Hiroyuki Ikezoe <poincare@ikezoe.net>
2009-10-06 11:54:46 +0900

    use getService instead of createInstance.

--- test/cpu.test.js
+++ test/cpu.test.js
@@ -11,28 +11,28 @@ function tearDown() {
 testCreate.description = "create instance test";
 testCreate.priority = 'must';
 function testCreate() {
-  gCPU = Cc["@clear-code.com/cpu/monitor;1"].createInstance(Ci.clICPUMonitor);
+  gCPU = Cc["@clear-code.com/cpu/monitor;1"].getService(Ci.clICPUMonitor);
   assert.isDefined(gCPU);
 }
 
 testUser.description = "user property test";
 testUser.priority = 'must';
 function testUser() {
-  gCPU = Cc["@clear-code.com/cpu/monitor;1"].createInstance(Ci.clICPUMonitor);
+  gCPU = Cc["@clear-code.com/cpu/monitor;1"].getService(Ci.clICPUMonitor);
   assert.equal(0, gCPU.user);
 }
 
 testSystem.description = "system property test";
 testSystem.priority = 'must';
 function testSystem() {
-  gCPU = Cc["@clear-code.com/cpu/monitor;1"].createInstance(Ci.clICPUMonitor);
+  gCPU = Cc["@clear-code.com/cpu/monitor;1"].getService(Ci.clICPUMonitor);
   assert.equal(0, gCPU.system);
 }
 
 testIdle.description = "idle property test";
 testIdle.priority = 'must';
 function testIdle() {
-  gCPU = Cc["@clear-code.com/cpu/monitor;1"].createInstance(Ci.clICPUMonitor);
+  gCPU = Cc["@clear-code.com/cpu/monitor;1"].getService(Ci.clICPUMonitor);
   assert.equal(0, gCPU.idle);
 }
 
