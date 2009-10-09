ded725cc572a33df2d8e8f743278dbc03d74a26c
Hiroyuki Ikezoe <ikezoe@clear-code.com>
2009-10-09 15:07:12 +0900

    remove an accidental commit.

--- test/system.test.js
+++ test/system.test.js
@@ -24,6 +24,5 @@ testGetService.priority = 'must';
 function testGetService() {
   let systemService = Cc["@clear-code.com/system;1"].getService(Ci.clISystem);
   assert.isDefined(systemService);
-  assert.isDefined(systemService.cpu);
 }
 
