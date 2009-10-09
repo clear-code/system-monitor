50459479b973f7e6742380b24d82afe26c1bb28b
Hiroyuki Ikezoe <ikezoe@clear-code.com>
2009-10-09 10:06:02 +0900

    remove clICPUMonitor.
    This interface was replaced by clICPU.

--- components/clSystemMonitorModule.cpp
+++ components/clSystemMonitorModule.cpp
@@ -10,11 +10,9 @@
 #endif
 
 #include "clCPU.h"
-#include "clCPUMonitor.h"
 #include "clSystem.h"
 
 NS_GENERIC_FACTORY_CONSTRUCTOR(clCPU)
-NS_GENERIC_FACTORY_CONSTRUCTOR(clCPUMonitor)
 NS_GENERIC_FACTORY_CONSTRUCTOR(clSystem)
 
 static NS_METHOD
@@ -37,28 +35,6 @@ registerSystem(nsIComponentManager *aCompMgr,
     return NS_OK;
 }
 
-static NS_METHOD
-registerCPUMonitor(nsIComponentManager *aCompMgr,
-                   nsIFile *aPath,
-                   const char *registryLocation,
-                   const char *componentType,
-                   const nsModuleComponentInfo *info)
-{
-    nsresult rv;
-    nsCOMPtr<nsICategoryManager> catMan(do_GetService(NS_CATEGORYMANAGER_CONTRACTID, &rv));
-    if (NS_FAILED(rv))
-        return rv;
-
-    catMan->AddCategoryEntry(JAVASCRIPT_GLOBAL_PROPERTY_CATEGORY,
-                             "monitor",
-                             CL_CPU_MONITOR_CONTRACT_ID,
-                             PR_TRUE, PR_TRUE,
-                             nsnull);
-    return NS_OK;
-}
-
-NS_DECL_CLASSINFO(clCPUMonitor)
-
 static nsModuleComponentInfo systemComponents[] =
 {
     {
@@ -73,18 +49,6 @@ static nsModuleComponentInfo systemComponents[] =
        CL_CPU_CID,
        CL_CPU_CONTRACT_ID,
        clCPUConstructor
-    },
-    {
-       "CPU Monitor",
-       CL_CPU_MONITOR_CID,
-       CL_CPU_MONITOR_CONTRACT_ID,
-       clCPUMonitorConstructor,
-       registerCPUMonitor,
-       NULL,
-       NULL,
-       NS_CI_INTERFACE_GETTER_NAME(clCPUMonitor),
-       NULL,
-       &NS_CLASSINFO_NAME(clCPUMonitor)
     }
 };
 
