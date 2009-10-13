a789e9d5fe5b22f15d017919d65ec413922837f3
Hiroyuki Ikezoe <ikezoe@clear-code.com>
2009-10-14 07:58:44 +0900

    cosmetic fix.

--- components/clCPUTime.cpp
+++ components/clCPUTime.cpp
@@ -16,35 +16,40 @@ clCPUTime::~clCPUTime()
 }
 
 /* readonly attribute double user; */
-NS_IMETHODIMP clCPUTime::GetUser(double *aUser)
+NS_IMETHODIMP
+clCPUTime::GetUser(double *aUser)
 {
     *aUser = mUser;
     return NS_OK;
 }
 
 /* readonly attribute double nice; */
-NS_IMETHODIMP clCPUTime::GetNice(double *aNice)
+NS_IMETHODIMP
+clCPUTime::GetNice(double *aNice)
 {
     *aNice = mNice;
     return NS_OK;
 }
 
 /* readonly attribute double system; */
-NS_IMETHODIMP clCPUTime::GetSystem(double *aSystem)
+NS_IMETHODIMP
+clCPUTime::GetSystem(double *aSystem)
 {
     *aSystem = mSystem;
     return NS_OK;
 }
 
 /* readonly attribute double idle; */
-NS_IMETHODIMP clCPUTime::GetIdle(double *aIdle)
+NS_IMETHODIMP
+clCPUTime::GetIdle(double *aIdle)
 {
     *aIdle = mIdle;
     return NS_OK;
 }
 
 /* readonly attribute double io_wait; */
-NS_IMETHODIMP clCPUTime::GetIo_wait(double *aIOWait)
+NS_IMETHODIMP
+clCPUTime::GetIo_wait(double *aIOWait)
 {
     *aIOWait = mIOWait;
     return NS_OK;
