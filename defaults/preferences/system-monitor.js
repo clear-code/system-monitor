94c5cc3cb99e43483e15e99410166c890c684e40
Piro / SHIMODA Hiroshi <piro@p.club.ne.jp>
2010-09-23 04:33:08 +0900

    change default color of graphs

--- defaults/preferences/system-monitor.js
+++ defaults/preferences/system-monitor.js
@@ -4,11 +4,11 @@ pref("extensions.system-monitor@clear-code.com.cpu-usage.initialShow", false);
 pref("extensions.system-monitor@clear-code.com.cpu-usage.size", 48);
 pref("extensions.system-monitor@clear-code.com.cpu-usage.interval", 1000);
 pref("extensions.system-monitor@clear-code.com.cpu-usage.color.background", "#000000");
-pref("extensions.system-monitor@clear-code.com.cpu-usage.color.foreground", "#008000");
+pref("extensions.system-monitor@clear-code.com.cpu-usage.color.foreground", "#33CC00");
 
 pref("extensions.system-monitor@clear-code.com.memory-usage.initialShow", false);
 pref("extensions.system-monitor@clear-code.com.memory-usage.size", 48);
 pref("extensions.system-monitor@clear-code.com.memory-usage.interval", 1000);
 pref("extensions.system-monitor@clear-code.com.memory-usage.color.background", "#000000");
-pref("extensions.system-monitor@clear-code.com.memory-usage.color.foreground", "#008080");
+pref("extensions.system-monitor@clear-code.com.memory-usage.color.foreground", "#00CCCC");
 
