14f77c60e748f85bae1293b1713e752ac9627b56
SHIMODA Hiroshi <shimoda@clear-code.com>
2010-09-24 19:19:59 +0900

    rewrite registration module for Gecko 2.0 (see: https://developer.mozilla.org/en/XPCOM/XPCOM_changes_in_Gecko_2.0 )

--- components/clSystemModule.cpp
+++ components/clSystemModule.cpp
@@ -1,4 +1,5 @@
-#include <nsIGenericFactory.h>
+#include <mozilla/ModuleUtils.h>
+
 #include <nsICategoryManager.h>
 #include <nsServiceManagerUtils.h>
 #include <nsIClassInfoImpl.h>
@@ -15,81 +16,39 @@ NS_GENERIC_FACTORY_SINGLETON_CONSTRUCTOR(clSystem, clSystem::GetService)
 NS_GENERIC_FACTORY_CONSTRUCTOR(clCPUTime)
 NS_GENERIC_FACTORY_CONSTRUCTOR(clMemory)
 
-NS_DECL_CLASSINFO(clCPU)
-NS_DECL_CLASSINFO(clSystem)
-NS_DECL_CLASSINFO(clCPUTime)
-NS_DECL_CLASSINFO(clMemory)
+NS_DEFINE_NAMED_CID(CL_SYSTEM_CID)
+NS_DEFINE_NAMED_CID(CL_CPU_CID)
+NS_DEFINE_NAMED_CID(CL_CPU_TIME_CID)
+NS_DEFINE_NAMED_CID(CL_MEMORY_CID)
 
-static NS_METHOD
-registerSystem(nsIComponentManager *aCompMgr,
-               nsIFile *aPath,
-               const char *registryLocation,
-               const char *componentType,
-               const nsModuleComponentInfo *info)
-{
-    nsresult rv;
-    nsCOMPtr<nsICategoryManager> catMan(do_GetService(NS_CATEGORYMANAGER_CONTRACTID, &rv));
-    if (NS_FAILED(rv))
-        return rv;
-
-    catMan->AddCategoryEntry(JAVASCRIPT_GLOBAL_PROPERTY_CATEGORY,
-                             "system",
-                             CL_SYSTEM_CONTRACT_ID,
-                             PR_TRUE, PR_TRUE,
-                             nsnull);
-    return NS_OK;
-}
-
-static nsModuleComponentInfo systemComponents[] =
-{
-    {
-       "System Property",
-       CL_SYSTEM_CID,
-       CL_SYSTEM_CONTRACT_ID,
-       clSystemConstructor,
-       registerSystem,
-       NULL,
-       NULL,
-       NS_CI_INTERFACE_GETTER_NAME(clSystem),
-       NULL,
-       &NS_CLASSINFO_NAME(clSystem)
-    },
-    {
-       "CPU",
-       CL_CPU_CID,
-       CL_CPU_CONTRACT_ID,
-       clCPUConstructor,
-       NULL,
-       NULL,
-       NULL,
-       NS_CI_INTERFACE_GETTER_NAME(clCPU),
-       NULL,
-       &NS_CLASSINFO_NAME(clCPU)
-    },
-    {
-       "CPUTime",
-       CL_CPU_TIME_CID,
-       CL_CPU_TIME_CONTRACT_ID,
-       clCPUTimeConstructor,
-       NULL,
-       NULL,
-       NULL,
-       NS_CI_INTERFACE_GETTER_NAME(clCPUTime),
-       NULL,
-       &NS_CLASSINFO_NAME(clCPUTime)
-    },
-    {
-       "Memory",
-       CL_MEMORY_CID,
-       CL_MEMORY_CONTRACT_ID,
-       clMemoryConstructor,
-       NULL,
-       NULL,
-       NULL,
-       NS_CI_INTERFACE_GETTER_NAME(clMemory),
-       NULL,
-       &NS_CLASSINFO_NAME(clMemory)
-    },
+static const mozilla::Module::CIDEntry kSystemCIDs[] = {
+  { &kCL_SYSTEM_CID, false, NULL, clSystemConstructor },
+  { &kCL_CPU_CID, false, NULL, clCPUConstructor },
+  { &kCL_CPU_TIME_CID, false, NULL, clCPUTimeConstructor },
+  { &kCL_MEMORY_CID, false, NULL, clMemoryConstructor },
+  { NULL }
 };
 
-NS_IMPL_NSGETMODULE(clSystemMonitorModule, systemComponents);
+static const mozilla::Module::ContractIDEntry kSystemContracts[] = {
+  { CL_SYSTEM_CONTRACT_ID, &kCL_SYSTEM_CID },
+  { CL_CPU_CONTRACT_ID, &kCL_CPU_CID },
+  { CL_CPU_TIME_CONTRACT_ID, &kCL_CPU_TIME_CID },
+  { CL_MEMORY_CONTRACT_ID, &kCL_MEMORY_CID },
+  { NULL }
+};
+
+static const mozilla::Module::CategoryEntry kSystemCategories[] = {
+  { "JavaScript global property", "system", CL_SYSTEM_CID },
+  { NULL }
+};
+
+static const mozilla::Module kSystemModule = {
+  mozilla::Module::kVersion,
+  kSystemCIDs,
+  kSystemContracts,
+  kSystemCategories
+};
+
+NSMODULE_DEFN(clSystemModule) = &kSystemModule;
+
+NS_IMPL_MOZILLA192_NSGETMODULE(&kSystemModule)
