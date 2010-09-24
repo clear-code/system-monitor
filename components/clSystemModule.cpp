16f083ce75fe7119364310e5169aad6e7b4b5f2b
Piro / SHIMODA Hiroshi <piro@p.club.ne.jp>
2010-09-25 04:45:44 +0900

    sort arrays

--- components/clSystemModule.cpp
+++ components/clSystemModule.cpp
@@ -1,34 +1,35 @@
 #include <mozilla-config.h>
 #include <mozilla/ModuleUtils.h>
+#include <nsIClassInfoImpl.h>
 
 #include "clCPU.h"
-#include "clSystem.h"
 #include "clCPUTime.h"
 #include "clMemory.h"
+#include "clSystem.h"
 
 NS_GENERIC_FACTORY_CONSTRUCTOR(clCPU)
 NS_GENERIC_FACTORY_CONSTRUCTOR(clCPUTime)
 NS_GENERIC_FACTORY_CONSTRUCTOR(clMemory)
 NS_GENERIC_FACTORY_SINGLETON_CONSTRUCTOR(clSystem, clSystem::GetService)
 
-NS_DEFINE_NAMED_CID(CL_SYSTEM_CID);
 NS_DEFINE_NAMED_CID(CL_CPU_CID);
 NS_DEFINE_NAMED_CID(CL_CPU_TIME_CID);
 NS_DEFINE_NAMED_CID(CL_MEMORY_CID);
+NS_DEFINE_NAMED_CID(CL_SYSTEM_CID);
 
 static const mozilla::Module::CIDEntry kSystemCIDs[] = {
-  { &kCL_SYSTEM_CID, false, NULL, clSystemConstructor },
   { &kCL_CPU_CID, false, NULL, clCPUConstructor },
   { &kCL_CPU_TIME_CID, false, NULL, clCPUTimeConstructor },
   { &kCL_MEMORY_CID, false, NULL, clMemoryConstructor },
+  { &kCL_SYSTEM_CID, true, NULL, clSystemConstructor },
   { NULL }
 };
 
 static const mozilla::Module::ContractIDEntry kSystemContracts[] = {
-  { CL_SYSTEM_CONTRACT_ID, &kCL_SYSTEM_CID },
   { CL_CPU_CONTRACT_ID, &kCL_CPU_CID },
   { CL_CPU_TIME_CONTRACT_ID, &kCL_CPU_TIME_CID },
   { CL_MEMORY_CONTRACT_ID, &kCL_MEMORY_CID },
+  { CL_SYSTEM_CONTRACT_ID, &kCL_SYSTEM_CID },
   { NULL }
 };
 
@@ -46,4 +47,4 @@ static const mozilla::Module kSystemModule = {
 
 NSMODULE_DEFN(clSystemModule) = &kSystemModule;
 
-NS_IMPL_MOZILLA192_NSGETMODULE(&kSystemModule);
+NS_IMPL_MOZILLA192_NSGETMODULE(&kSystemModule)
