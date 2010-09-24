9f71a94cf8f51810ad923cbd6a081a1511f378e8
Piro / SHIMODA Hiroshi <piro@p.club.ne.jp>
2010-09-25 04:46:50 +0900

    insert items to the nav-bar if the menu-bar is hidden (part 2)

--- defaults/preferences/system-monitor.js
+++ defaults/preferences/system-monitor.js
@@ -1,4 +1,4 @@
-pref("extensions.system-monitor@clear-code.com.defaultTargetToolbar", "toolbar-menubar");
+pref("extensions.system-monitor@clear-code.com.defaultTargetToolbar", "toolbar-menubar,nav-bar");
 
 pref("extensions.system-monitor@clear-code.com.cpu-usage.initialShow", false);
 pref("extensions.system-monitor@clear-code.com.cpu-usage.size", 48);
