c9d8d011f85c827c79c95b5c8c62e03d12d0691e
Piro / SHIMODA Hiroshi <piro@p.club.ne.jp>
2010-09-30 23:49:54 +0900

    split platform dependent implementations to each file

--- components/clMemory.cpp
+++ components/clMemory.cpp
@@ -3,112 +3,45 @@
 #include <nsIClassInfoImpl.h>
 #include <nsMemory.h>
 
-#ifdef HAVE_LIBGTOP2
-#include <glibtop/mem.h>
-#elif defined(XP_WIN)
-#include <windows.h>
-#elif defined(XP_MACOSX)
-#include <mach/mach_init.h>
-#include <mach/host_info.h>
-#include <mach/mach_host.h>
-#endif
-
 NS_IMPL_ISUPPORTS2_CI(clMemory,
                       clIMemory,
                       nsISecurityCheckedComponent)
 
 clMemory::clMemory()
+    : mMemory(CL_GetMemory())
 {
-  /* member initializers and constructor code */
-#ifdef HAVE_LIBGTOP2
-#define CL_MEMORY_AVAILABLE_TOTAL
-#define CL_MEMORY_AVAILABLE_USED
-#define CL_MEMORY_AVAILABLE_FREE
-  glibtop_mem memory;
-  glibtop_get_mem(&memory);
-
-  mTotal = memory.total;
-  mUsed = memory.used - memory.cached;
-  mFree = mTotal - mUsed;
-#elif defined(XP_WIN)
-#define CL_MEMORY_AVAILABLE_TOTAL
-#define CL_MEMORY_AVAILABLE_USED
-#define CL_MEMORY_AVAILABLE_FREE
-#define CL_MEMORY_AVAILABLE_VIRTUAL_USED
-  MEMORYSTATUSEX memory;
-  memory.dwLength=sizeof(memory);
-  GlobalMemoryStatusEx(&memory);
-
-  mTotal = memory.ullTotalPhys;
-  mFree = memory.ullAvailPhys;
-  mUsed = mTotal - mFree;
-  mVirtualUsed = memory.ullTotalVirtual - memory.ullAvailVirtual;
-#elif defined(XP_MACOSX)
-#define CL_MEMORY_AVAILABLE_TOTAL
-#define CL_MEMORY_AVAILABLE_USED
-#define CL_MEMORY_AVAILABLE_FREE
-  host_basic_info total_memory;
-  mach_msg_type_number_t total_count = HOST_BASIC_INFO_COUNT;
-  host_info(mach_host_self(), HOST_BASIC_INFO, (host_info_t) &total_memory, &total_count);
-
-  mTotal = total_memory.memory_size;
-
-  vm_statistics memory;
-  mach_msg_type_number_t count = HOST_VM_INFO_COUNT;
-  host_statistics(mach_host_self(), HOST_VM_INFO, (host_info_t) &memory, &count);
-
-  mFree = memory.free_count * vm_page_size;
-  mUsed = mTotal - mFree;
-#endif
 }
 
 clMemory::~clMemory()
 {
-  /* destructor code */
 }
 
 /* readonly attribute PRUint64 total; */
 NS_IMETHODIMP clMemory::GetTotal(PRUint64 *aTotal)
 {
-#ifdef CL_MEMORY_AVAILABLE_TOTAL
-    *aTotal = mTotal;
-    return NS_OK;
-#else
-    return NS_ERROR_NOT_IMPLEMENTED;
-#endif
+    *aTotal = mMemory.total;
+    return aTotal < 0 ? NS_ERROR_NOT_IMPLEMENTED : NS_OK;
 }
 
 /* readonly attribute PRUint64 used; */
 NS_IMETHODIMP clMemory::GetUsed(PRUint64 *aUsed)
 {
-#ifdef CL_MEMORY_AVAILABLE_USED
-    *aUsed = mUsed;
-    return NS_OK;
-#else
-    return NS_ERROR_NOT_IMPLEMENTED;
-#endif
+    *aUsed = mMemory.used;
+    return aUsed < 0 ? NS_ERROR_NOT_IMPLEMENTED : NS_OK;
 }
 
 /* readonly attribute PRUint64 free; */
 NS_IMETHODIMP clMemory::GetFree(PRUint64 *aFree)
 {
-#ifdef CL_MEMORY_AVAILABLE_FREE
-    *aFree = mFree;
-    return NS_OK;
-#else
-    return NS_ERROR_NOT_IMPLEMENTED;
-#endif
+    *aFree = mMemory.free;
+    return aFree < 0 ? NS_ERROR_NOT_IMPLEMENTED : NS_OK;
 }
 
 /* readonly attribute PRUint64 virtualUsed; */
 NS_IMETHODIMP clMemory::GetVirtualUsed(PRUint64 *aVirtualUsed)
 {
-#ifdef CL_MEMORY_AVAILABLE_VIRTUAL_USED
-    *aVirtualUsed = mVirtualUsed;
-    return NS_OK;
-#else
-    return NS_ERROR_NOT_IMPLEMENTED;
-#endif
+    *aVirtualUsed = mMemory.virtualUsed;
+    return aVirtualUsed < 0 ? NS_ERROR_NOT_IMPLEMENTED : NS_OK;
 }
 
 static char *
--- components/clMemory.h
+++ components/clMemory.h
@@ -10,6 +10,17 @@
 #include "clIMemory.h"
 
 #include <nsISecurityCheckedComponent.h>
+
+typedef struct CL_Memory CL_Memory;
+struct CL_Memory {
+    PRUint64 total;
+    PRUint64 free;
+    PRUint64 used;
+    PRUint64 virtualUsed;
+};
+
+extern CL_Memory CL_GetMemory();
+
 #define CL_MEMORY_CONTRACT_ID "@clear-code.com/system/memory;1"
 #define CL_MEMORY_CID {0x32404cb9, 0x1d05, 0x4f80, {0x83, 0x88, 0x4f, 0x6a, 0xc2, 0x15, 0x76, 0x0b}}
 
@@ -27,10 +38,7 @@ private:
   ~clMemory();
 
 protected:
-  PRUint64 mTotal;
-  PRUint64 mUsed;
-  PRUint64 mFree;
-  PRUint64 mVirtualUsed;
+  CL_Memory mMemory;
 };
 
 #endif /* __CL_MEMORY_H__ */
