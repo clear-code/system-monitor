21dc0bd642625559ef25110338e8e6aef99c645e
下田洋志 <piro@clear-mac.local>
2010-10-04 10:35:23 +0900

    PRUInt64 => PRUint64

--- components/clMemoryDarwin.cpp
+++ components/clMemoryDarwin.cpp
@@ -11,13 +11,13 @@ CL_GetMemory()
     mach_msg_type_number_t total_count = HOST_BASIC_INFO_COUNT;
     host_info(mach_host_self(), HOST_BASIC_INFO, (host_info_t) &total_memory, &total_count);
 
-    PRUInt64 total = total_memory.memory_size;
+    PRUint64 total = total_memory.memory_size;
 
     vm_statistics memory;
     mach_msg_type_number_t count = HOST_VM_INFO_COUNT;
     host_statistics(mach_host_self(), HOST_VM_INFO, (host_info_t) &memory, &count);
 
-    PRUInt64 free = memory.free_count * vm_page_size;
+    PRUint64 free = memory.free_count * vm_page_size;
 
     CL_Memory info = {
         total,        // total
