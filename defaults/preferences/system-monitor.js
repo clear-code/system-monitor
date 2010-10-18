86a4194c2f37c2f81a76df806b645c4e38d6078c
Piro / SHIMODA Hiroshi <piro@p.club.ne.jp>
2010-10-19 08:30:40 +0900

    change default alpha of the gradient from 0.5 to 0.8

--- defaults/preferences/system-monitor.js
+++ defaults/preferences/system-monitor.js
@@ -5,7 +5,7 @@ pref("extensions.system-monitor@clear-code.com.cpu-usage.size", 48);
 pref("extensions.system-monitor@clear-code.com.cpu-usage.interval", 1000);
 pref("extensions.system-monitor@clear-code.com.cpu-usage.color.background", "#000000");
 pref("extensions.system-monitor@clear-code.com.cpu-usage.color.foreground", "#33CC00");
-pref("extensions.system-monitor@clear-code.com.cpu-usage.color.gradientEndAlpha", "0.5");
+pref("extensions.system-monitor@clear-code.com.cpu-usage.color.gradientEndAlpha", "0.8");
 // style flag for multiplex values:
 //   1 = unified, 2 = stacked, 4 = layered, 128 = polygonal
 pref("extensions.system-monitor@clear-code.com.cpu-usage.style", 4);
@@ -15,6 +15,6 @@ pref("extensions.system-monitor@clear-code.com.memory-usage.size", 48);
 pref("extensions.system-monitor@clear-code.com.memory-usage.interval", 1000);
 pref("extensions.system-monitor@clear-code.com.memory-usage.color.background", "#000000");
 pref("extensions.system-monitor@clear-code.com.memory-usage.color.foreground", "#00CCCC");
-pref("extensions.system-monitor@clear-code.com.memory-usage.color.gradientEndAlpha", "0.5");
+pref("extensions.system-monitor@clear-code.com.memory-usage.color.gradientEndAlpha", "0.8");
 pref("extensions.system-monitor@clear-code.com.memory-usage.style", 1);
 
