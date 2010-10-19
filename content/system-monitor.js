2d2f77bc7c0c2d545067ed15783b6bb446da0330
SHIMODA Hiroshi <shimoda@clear-code.com>
2010-10-19 12:58:31 +0900

    update graph dynamically when some prefs are changed

--- content/system-monitor.js
+++ content/system-monitor.js
@@ -308,15 +308,10 @@ SystemMonitorSimpleGraphItem.prototype = {
     this.size = this.getPref(this.domain+this.id+".size");
     this.interval = this.getPref(this.domain+this.id+".interval");
 
-    this.colorBackground = this.getPref(this.domain+this.id+".color.background");
-    this.colorForeground = this.getPref(this.domain+this.id+".color.foreground");
-
-    this.gradientEndAlpha = Number(this.getPref(this.domain+this.id+".color.gradientEndAlpha"));
-    if (isNaN(this.gradientEndAlpha))
-      this.gradientEndAlpha = 0.5;
-    this.gradientEndAlpha = Math.min(0.999999, Math.max(0, this.gradientEndAlpha));
-
-    this.style = this.getPref(this.domain+this.id+".style");
+    this.onChangePref(this.domain+this.id+".color.background");
+    this.onChangePref(this.domain+this.id+".color.foreground");
+    this.onChangePref(this.domain+this.id+".color.gradientEndAlpha");
+    this.onChangePref(this.domain+this.id+".style");
 
     var canvas = this.canvas;
     canvas.style.width = (canvas.width = item.width = this.size)+"px";
@@ -608,12 +603,35 @@ SystemMonitorSimpleGraphItem.prototype = {
     switch (aData.replace(this.domain+this.id+'.', '')) {
       case "size":
       case "interval":
-      case "color.background":
-      case "color.foreground":
-      case "color.gradientEndAlpha":
-      case "style":
         if (this.listening)
           this.update();
+        break;
+
+      case "color.background":
+        this.colorBackground = this.getPref(aData);
+        if (this.listening)
+          this.drawGraph(true);
+        break;
+
+      case "color.foreground":
+        this.colorForeground = this.getPref(aData);
+        if (this.listening)
+          this.drawGraph(true);
+        break;
+
+      case "color.gradientEndAlpha":
+        this.gradientEndAlpha = Number(this.getPref(aData));
+        if (isNaN(this.gradientEndAlpha))
+          this.gradientEndAlpha = 0.5;
+        this.gradientEndAlpha = Math.min(0.999999, Math.max(0, this.gradientEndAlpha));
+        if (this.listening)
+          this.drawGraph(true);
+        break;
+
+      case "style":
+        this.style = this.getPref(this.domain+this.id+".style");
+        if (this.listening)
+          this.drawGraph(true);
         break;
     }
   },
