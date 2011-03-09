f60ba713eaaab14e2483e314d9236d06a348f396
下田洋志 <piro@clear-mac.local>
2011-03-09 18:24:49 +0900

    add files for Darwin

---
+++ modules/Darwin/cpu.js
@@ -0,0 +1,47 @@
+var EXPORTED_SYMBOLS = ['getCount', 'getCPUTimes', 'calculateCPUUsage'];
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+
+Components.utils.import('resource://gre/modules/ctypes.jsm');
+Components.utils.import('resource://system-monitor-modules/shutdown-listener.js');
+
+const xxx = ctypes.open('');
+addShutdownListener(function() { xxx.close(); });
+
+
+function getCount() {
+	return 0;
+}
+
+function getCPUTimes() {
+	var times = [];
+	return times;
+}
+
+function calculateCPUUsage(aPrevious, aCurrent) {
+	var user   = aCurrent.user - aPrevious.user;
+	var system = aCurrent.system - aPrevious.system;
+	var nice   = aCurrent.nice - aPrevious.nice;
+	var idle   = aCurrent.idle - aPrevious.idle;
+
+	var total  = user + system + nice + idle;
+	if (total == 0) {
+		return {
+			user   : 0,
+			system : 0,
+			nice   : 0,
+			idle   : 0,
+			iowait : 0
+		};
+	}
+	else {
+		return {
+			user   : user / total,
+			system : system / total,
+			nice   : nice / total,
+			idle   : idle / total,
+			iowait : 0
+		};
+	}
+}
