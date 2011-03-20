5ef0431e86822ed016a1ac67c6d0330ef6d6f165
Piro / SHIMODA Hiroshi <piro@p.club.ne.jp>
2011-03-20 17:54:59 +0900

    fix layout of configuration dialog

--- content/config.js
+++ content/config.js
@@ -9,10 +9,6 @@ function initMemoryUsagePane() {
   updateAlphaUIFromPref('memory-usage.color.backgroundStartAlpha');
   updateAlphaUIFromPref('memory-usage.color.foregroundStartAlpha');
   updateAlphaUIFromPref('memory-usage.color.selfGlobalAlpha');
-
-  var self = document.getElementById('memory-usage-self-box');
-  if (!('@clear-code.com/system;2' in Components.classes))
-    self.setAttribute('hidden', true);
 }
 
 function updateStyleUIFromPref(aKey) {
