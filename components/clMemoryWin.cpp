91ba3fbfa0ef681ca3275570c35e53d13826ded8
SHIMODA Hiroshi <shimoda@clear-code.com>
2011-03-22 19:07:47 +0900

    calculate actual memory usage on Windows (Gecko 1.9.2)

--- components/clMemoryWin.cpp
+++ components/clMemoryWin.cpp
@@ -1,25 +1,8 @@
 #include "clMemory.h"
 
+#include <stdlib.h>
 #include <windows.h>
 #include <psapi.h>
-
-#if (_WIN32_WINNT < 0x0501)
-// for Windows 2000 - Windows XP SP1
-typedef PROCESS_MEMORY_COUNTERS_EX PROCESS_MEMORY_COUNTERS_EX;
-struct PROCESS_MEMORY_COUNTERS_EX {
-    DWORD cb;
-    DWORD PageFaultCount;
-    SIZE_T PeakWorkingSetSize;
-    SIZE_T WorkingSetSize;
-    SIZE_T QuotaPeakPagedPoolUsage;
-    SIZE_T QuotaPagedPoolUsage;
-    SIZE_T QuotaPeakNonPagedPoolUsage;
-    SIZE_T QuotaNonPagedPoolUsage;
-    SIZE_T PagefileUsage;
-    SIZE_T PeakPagefileUsage;
-    SIZE_T PrivateUsage;
-};
-#endif
 
 CL_Memory
 CL_GetMemory()
@@ -28,25 +11,38 @@ CL_GetMemory()
     memory.dwLength=sizeof(memory);
     GlobalMemoryStatusEx(&memory);
 
-    PROCESS_MEMORY_COUNTERS_EX self;
-    GetProcessMemoryInfo(GetCurrentProcess(),
-                         (PROCESS_MEMORY_COUNTERS*) &self,
-                         sizeof self);
+    SYSTEM_INFO systemInfo;
+    GetSystemInfo(&systemInfo);
 
-    PRUint64 self_usage;
-    if (self.PrivateUsage > 0) {
-      self_usage = self.PrivateUsage;
+    ULONG_PTR self[1];
+    QueryWorkingSet(GetCurrentProcess(),
+                    (PVOID*) &self,
+                    sizeof(self));
+
+    DWORD pagesCount = self[0];
+    ULONG_PTR *actualSelf = (ULONG_PTR *) calloc(pagesCount + 1, sizeof(ULONG_PTR));
+    QueryWorkingSet(GetCurrentProcess(),
+                    (PVOID*) actualSelf,
+                    (pagesCount + 1) * sizeof(ULONG_PTR));
+
+    PRUint64 selfUsage = 0;
+    for (ULONG_PTR i = 1; i < pagesCount; i++) {
+      if (actualSelf[i] == 0) {
+        break;
+      }
+      if (!(actualSelf[i] & 0x100)) {
+        selfUsage++;
+      }
     }
-    else {
-      self_usage = self.WorkingSetSize;
-    }
+
+    free(actualSelf);
 
     CL_Memory info = {
         memory.ullTotalPhys,                             // total
         memory.ullAvailPhys,                             // free
         memory.ullTotalPhys - memory.ullAvailPhys,       // used,
         memory.ullTotalVirtual - memory.ullAvailVirtual, // virtualUsed
-        self_usage                                       // self
+        selfUsage * systemInfo.dwPageSize               // self
     };
     return info;
 }
