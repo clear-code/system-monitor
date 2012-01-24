bb7dbb7d3dc8e62d50e8bec3abae355dcbb641c6
Piro / SHIMODA Hiroshi <piro@p.club.ne.jp>
2012-01-25 01:13:01 +0900

    delete debug message

--- modules/clSystem.js
+++ modules/clSystem.js
@@ -199,7 +199,6 @@ gCachedNativeAPI.init();
 
 
 function clSystem() {
-dump('clSystem initialize\n');
 	if (Services.vc.compare(Services.appinfo.platformVersion, '1.9.99') <= 0)
 		throw new Error('initialization error: JavaScript implementations are available on Gecko 2.0 o later.');
 
