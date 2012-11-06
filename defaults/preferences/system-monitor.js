041d1badcc38139e5e0c435680c2af9258a4df8b
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2012-11-07 05:11:42 +0900

    Change default color of the up bytes

--- defaults/preferences/system-monitor.js
+++ defaults/preferences/system-monitor.js
@@ -50,7 +50,7 @@ pref("extensions.system-monitor@clear-code.com.network-usage.color.foreground", 
 pref("extensions.system-monitor@clear-code.com.network-usage.color.foregroundStartAlpha", "0.4");
 pref("extensions.system-monitor@clear-code.com.network-usage.color.foregroundEndAlpha", "1");
 pref("extensions.system-monitor@clear-code.com.network-usage.color.foregroundMinAlpha", "0.2");
-pref("extensions.system-monitor@clear-code.com.network-usage.color.foreground.1", "#CCFF66");
+pref("extensions.system-monitor@clear-code.com.network-usage.color.foreground.1", "#FF6633");
 pref("extensions.system-monitor@clear-code.com.network-usage.color.redZone", "#FF9900");
 pref("extensions.system-monitor@clear-code.com.network-usage.redZone", 65536); // 64 * 1024 (64kbps)
 pref("extensions.system-monitor@clear-code.com.network-usage.logscale", false);
